a2b2dd6781f942c2e7263af675e5da6f1264f79a
yiuuucen <1784373887@qq.com>
2018-05-15 10:40:58 +0800

    work

--- 百度地图api案例/js/index3.js
+++ 百度地图api案例/js/index3.js
@@ -4,35 +4,90 @@ $(function(){
     $("#btn02").click(function(){
         map02();
     })
+
+    $("#switch").click(function(){
+        
+        if($(".tab ol").css("left")=='0px'){
+            
+            $(".tab ol").animate({
+                left:"-255px",
+            })
+        }else{
+            $(".tab ol").animate({
+                left:"0px",
+            })
+        }
+    })
+
+    function mydata(str){
+        var n,m;
+        // if(str.indexOf("，")>=0){
+        //     str=str.replace(/，/ig,',');
+        // }
+        str=str.replace(/，/ig,',');
+        n=str.split(",")[0];
+        m=str.split(",")[1];
+        return [n,m]
+    }
+
     function map02(){
 
+        var num1=$("input[my-data='num1']").val();
+        var num2=$("input[my-data='num2']").val();
+        var num3=$("input[my-data='num3']").val();
+        var num4=$("input[my-data='num4']").val();
+        var num5=$("input[my-data='num5']").val();
+        var num6=$("input[my-data='num6']").val();
+
+        var num7=$("input[my-data='num7']").val();
+        var num8=$("input[my-data='num8']").val();
+        var num9=$("input[my-data='num9']").val();
+        var num10=$("input[my-data='num10']").val();
         var num11=$("input[my-data='num11']").val();
         var num12=$("input[my-data='num12']").val();
 
-        var num13=$("input[my-data='num13']").val();
-        var num14=$("input[my-data='num14']").val();
+        var arr1=[num1,num2,num3,num4,num5,num6];
+        var arr2=[num7,num8,num9,num10,num11,num12];
+       
+        function change(arr){
+            for(var i=0;i<arr.length;i++){
+                if(arr[i]==''){
+                    for(var j=i+1;j<arr.length;j++){
+                        if(arr[j]!=''){
+                            arr[i]=arr[j]
+                            break;
+                        }
+                    }
+                }
+            }
+            for(var i=arr.length-1;i>=0;i--){
+                if(arr[i]==''){
+                    for(var j=i-1;j>=0;j--){
+                        if(arr[j]!=''){
+                            arr[i]=arr[j]
+                            break;
+                        }
+                    }
+                }
+            }
+            return arr;
+        }
+        change(arr1);
+        change(arr2);
+        
+        num1=arr1[0];
+        num2=arr1[1];
+        num3=arr1[2];
+        num4=arr1[3];
+        num5=arr1[4];
+        num6=arr1[5];
 
-        var num15=$("input[my-data='num15']").val();
-        var num16=$("input[my-data='num16']").val();
-
-        var num17=$("input[my-data='num17']").val();
-        var num18=$("input[my-data='num18']").val();
-
-        var num19=$("input[my-data='num19']").val();
-        var num20=$("input[my-data='num20']").val();
-
-        // if(num11==''&&num12==''&&num13==''&&num14==''&&num15==''&&num16==''&&num17==''&&num18==''&&num19==''&&num20==''){
-        //     num11==121.391;
-        //     num12==31.117;
-        //     num13==121.558;
-        //     num14==31.224;
-        //     num15==121.511;
-        //     num16==31.268;
-        //     num17==121.355;
-        //     num18==30.748;
-        //     num19==121.283;
-        //     num20==31.373;
-        // }
+        num7=arr2[0];
+        num8=arr2[1];
+        num9=arr2[2];
+        num10=arr2[3];
+        num11=arr2[4];
+        num12=arr2[5];
 
         var chart = echarts.init(document.getElementById('allmap2'));
             
@@ -40,129 +95,25 @@ $(function(){
                 center: [121.391,31.117],
                 zoom: 10,
                 roam: true,
-                mapStyle: {
-                    styleJson: [{
-                        "featureType": "water",
-                        "elementType": "all",
-                        "stylers": {
-                            "color": "#d1d1d1"
-                        }
-                    }, {
-                        "featureType": "land",
-                        "elementType": "all",
-                        "stylers": {
-                            "color": "#f3f3f3"
-                        }
-                    }, {
-                        "featureType": "boundary",
-                        "elementType": "geometry",
-                        "stylers": {
-                            "color": "#979797"
-                        }
-                    }, {
-                        "featureType": "railway",
-                        "elementType": "all",
-                        "stylers": {
-                            "visibility": "off"
-                        }
-                    }, {
-                        "featureType": "highway",
-                        "elementType": "geometry",
-                        "stylers": {
-                            "color": "#ffffff"
-                        }
-                    }, {
-                        "featureType": "highway",
-                        "elementType": "geometry.fill",
-                        "stylers": {
-                            "color": "#979797",
-                            "lightness": 1
-                        }
-                    }, {
-                        "featureType": "highway",
-                        "elementType": "labels",
-                        "stylers": {
-                            "visibility": "off"
-                        }
-                    }, {
-                        "featureType": "arterial",
-                        "elementType": "geometry",
-                        "stylers": {
-                            "color": "#ffffff"
-                        }
-                    }, {
-                        "featureType": "arterial",
-                        "elementType": "geometry.fill",
-                        "stylers": {
-                            "color": "#ffffff"
-                        }
-                    }, {
-                        "featureType": "poi",
-                        "elementType": "all",
-                        "stylers": {
-                            "visibility": "off"
-                        }
-                    }, {
-                        "featureType": "gray",
-                        "elementType": "all",
-                        "stylers": {
-                            "color": "#979797",
-                            "visibility": "off"
-                        }
-                    }, {
-                        "featureType": "subway",
-                        "elementType": "all",
-                        "stylers": {
-                            "visibility": "off"
-                        }
-                    }, {
-                        "featureType": "manmade",
-                        "elementType": "all",
-                        "stylers": {
-                            "visibility": "off"
-                        }
-                    }, {
-                        "featureType": "local",
-                        "elementType": "all",
-                        "stylers": {
-                            "visibility": "true"
-                        }
-                    }, {
-                        "featureType": "arterial",
-                        "elementType": "labels",
-                        "stylers": {
-                            "visibility": "off"
-                        }
-                    }, {
-                        "featureType": "boundary",
-                        "elementType": "geometry.fill",
-                        "stylers": {
-                            "color": "#979797"
-                        }
-                    }, {
-                        "featureType": "building",
-                        "elementType": "all",
-                        "stylers": {
-                            "color": "#979797"
-                        }
-                    }, {
-                        "featureType": "label",
-                        "elementType": "all",
-                        "stylers": {
-                            "visibility": "true"
-                        }
-                    }]
-                }
             }
 
         var res=[
-            [{coord:[num11,num12]},{coord:[num13,num14]}],
-            [{coord:[num13,num14]},{coord:[num15,num16]}],
-            [{coord:[num15,num16]},{coord:[num17,num18]}],
-            [{coord:[num17,num18]},{coord:[num19,num20]}]
+            [{coord:[mydata(num1)[0],mydata(num1)[1]]},{coord:[mydata(num2)[0],mydata(num2)[1]]}],
+            [{coord:[mydata(num2)[0],mydata(num2)[1]]},{coord:[mydata(num3)[0],mydata(num3)[1]]}],
+            [{coord:[mydata(num3)[0],mydata(num3)[1]]},{coord:[mydata(num4)[0],mydata(num4)[1]]}],
+            [{coord:[mydata(num4)[0],mydata(num4)[1]]},{coord:[mydata(num5)[0],mydata(num5)[1]]}],
+            [{coord:[mydata(num5)[0],mydata(num5)[1]]},{coord:[mydata(num6)[0],mydata(num6)[1]]}]
         ];
-        var color = ['#237cc8', '#237cc8', '#237cc8'];
+        var res2=[
+            [{coord:[mydata(num7)[0],mydata(num7)[1]]},{coord:[mydata(num8)[0],mydata(num8)[1]]}],
+            [{coord:[mydata(num8)[0],mydata(num8)[1]]},{coord:[mydata(num9)[0],mydata(num9)[1]]}],
+            [{coord:[mydata(num9)[0],mydata(num9)[1]]},{coord:[mydata(num10)[0],mydata(num10)[1]]}],
+            [{coord:[mydata(num10)[0],mydata(num10)[1]]},{coord:[mydata(num11)[0],mydata(num11)[1]]}],
+            [{coord:[mydata(num11)[0],mydata(num11)[1]]},{coord:[mydata(num12)[0],mydata(num12)[1]]}]
+        ];
+        var color = ['#237cc8', '#f4a942'];
         var series = [];
+        var series2 = [];
 
         series=[
             {
@@ -178,7 +129,7 @@ $(function(){
                 },
                 lineStyle: {
                     normal: {
-                        color: color[1],
+                        color: color[0],
                         width: 0,
                         curveness: 0.2
                     }
@@ -198,7 +149,7 @@ $(function(){
                 },
                 lineStyle: {
                     normal: {
-                        color: color[1],
+                        color: color[0],
                         width: 1,
                         opacity: 0.4,
                         curveness: 0.2
@@ -225,12 +176,80 @@ $(function(){
                 },
                 itemStyle: {
                     normal: {
+                        color: color[0]
+                    }
+                },
+                data : [{value:[mydata(num1)[0],mydata(num1)[1],80]},{value:[mydata(num2)[0],mydata(num2)[1],80]},{value:[mydata(num3)[0],mydata(num3)[1],80]},{value:[mydata(num4)[0],mydata(num4)[1],80]},{value:[mydata(num5)[0],mydata(num5)[1],80]},{value:[mydata(num6)[0],mydata(num6)[1],80]}]    
+            },
+
+            // 22222222222222222222222222
+            {
+                type: 'lines',
+                coordinateSystem: 'bmap',
+                zlevel: 1,
+                effect: {
+                    show: true,
+                    period: 6,
+                    trailLength: 0.7,
+                    color: '#fff',
+                    symbolSize: 3
+                },
+                lineStyle: {
+                    normal: {
+                        color: color[1],
+                        width: 0,
+                        curveness: 0.2
+                    }
+                },
+                data: res2
+            },
+            {
+                type: 'lines',
+                coordinateSystem: 'bmap',
+                zlevel: 2,
+                effect: {
+                    show: true,
+                    period: 6,
+                    trailLength: 0,
+                    symbol: "triangle",
+                    symbolSize: 8
+                },
+                lineStyle: {
+                    normal: {
+                        color: color[1],
+                        width: 1,
+                        opacity: 0.4,
+                        curveness: 0.2
+                    }
+                },
+                data:res2
+            },
+            {
+                type: 'effectScatter',
+                coordinateSystem: 'bmap',
+                zlevel: 2,
+                rippleEffect: {
+                    brushType: 'stroke'
+                },
+                label: {
+                    normal: {
+                        show: true,
+                        position: 'right',
+                        formatter: '{b}'
+                    }
+                },
+                symbolSize: function (val) {
+                     return val[2] / 8;
+                },
+                itemStyle: {
+                    normal: {
                         color: color[1]
                     }
                 },
-                data : [{value:[num11,num12,80]},{value:[num13,num14,80]},{value:[num15,num16,80]},{value:[num17,num18,80]},{value:[num19,num20,80]}]    
+                data : [{value:[mydata(num7)[0],mydata(num7)[1],80]},{value:[mydata(num8)[0],mydata(num8)[1],80]},{value:[mydata(num9)[0],mydata(num9)[1],80]},{value:[mydata(num10)[0],mydata(num10)[1],80]},{value:[mydata(num11)[0],mydata(num11)[1],80]},{value:[mydata(num12)[0],mydata(num12)[1],80]}]    
             }
         ]
+
 
         option ={
             bmap:bmap,
@@ -255,6 +274,7 @@ $(function(){
             },
             series: series
         }
+
         chart.setOption(option);
     }
 
